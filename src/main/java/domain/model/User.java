4a85853a1c4ef91b8037831d8945dc22eb961fe4
jenilson25francisco <franciscoviwaldo@gmail.com>
2025-01-17 17:10:13 +0100

    feat: add users entity

---
+++ src/main/java/domain/model/User.java
@@ -0,0 +1,25 @@
+package domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String name;
+    private String email;
+    private String telefone;
+
+}
