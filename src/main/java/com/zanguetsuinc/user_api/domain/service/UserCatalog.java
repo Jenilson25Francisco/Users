ed088eb50c1caf0de4626ae5c782588882075fd6
jenilson25francisco <franciscoviwaldo@gmail.com>
2025-01-17 17:38:45 +0100

    feat: add user service and adding model mapper dependency

---
+++ src/main/java/com/zanguetsuinc/user_api/domain/service/UserCatalog.java
@@ -0,0 +1,39 @@
+package com.zanguetsuinc.user_api.domain.service;
+
+import com.zanguetsuinc.user_api.domain.model.User;
+import com.zanguetsuinc.user_api.domain.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class UserCatalog {
+
+    private final UserRepository userRepository;
+
+    public User get(Long userId){
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("cliente nao encontrado"));
+    }
+
+    @Transactional
+    public User creat(User user){
+
+        boolean usedEmail = userRepository.findByEmail(user.getEmail())
+                .stream()
+                .anyMatch(existClient -> !existClient.equals(user));
+
+        if (usedEmail){
+            throw new RuntimeException("este email ja existe");
+        }
+
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public void remove(Long userId){
+        userRepository.deleteById(userId);
+    }
+
+}
